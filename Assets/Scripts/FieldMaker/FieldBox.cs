555611df3472b74c972f8933d7ea0b0fdf353493
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-06-12 14:51:24 +0900

    [add]FieldBox作成

---
+++ Assets/Scripts/FieldMaker/FieldBox.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldBox : MonoBehaviour
+{
+    public bool _isPut = false;
+    public Transform _objectCreationPosition;
+}
