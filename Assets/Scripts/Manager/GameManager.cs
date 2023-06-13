d56b9a5384c53ae911e92a7780bf97e40aa1ea27
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-06-13 19:50:31 +0900

    [add]GameManager作成

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorTreeNodeGraphEditor;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerController _player;
+    Context _context;
+}
