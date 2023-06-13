76931aab5e4b3892141e9c39174aaf477dd863b4
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-06-13 15:26:02 +0900

    [add]BattleManager作成

---
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    
+    
+    private PlayerStatus _playerStatus;
+    private EnemyStatus _enemyStatus;
+}
