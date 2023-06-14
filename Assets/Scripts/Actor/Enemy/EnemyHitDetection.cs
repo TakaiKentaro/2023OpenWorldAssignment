4d279b2c11afa51f3e993e53abaa13b4453e9900
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-06-14 23:09:04 +0900

    [add]EnemyHitDetection作成

---
+++ Assets/Scripts/Actor/Enemy/EnemyHitDetection.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHitDetection : MonoBehaviour
+{
+    [SerializeField] private float _knockbackForce = 10f;
+    [SerializeField] private float _knockbackDuration = 0.5f;
+
+    private NavMeshAgent _agent;
+    private float _speed;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _speed = _agent.speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            Vector3 knockbackDirection = -transform.forward;
+            _agent.velocity = knockbackDirection * _knockbackForce;
+            StartCoroutine(ResetVelocity());
+        }
+    }
+
+    private IEnumerator ResetVelocity()
+    {
+        yield return new WaitForSeconds(_knockbackDuration);
+        _agent.velocity = Vector3.zero;
+        _agent.speed = _speed;
+    }
+}
