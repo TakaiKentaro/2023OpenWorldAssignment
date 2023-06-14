6c6b4443484ebf01b40bb17759596553b17c4632
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-06-14 23:07:48 +0900

    [add]PlayerAttack作成

---
+++ Assets/Scripts/Actor/Player/PlayerAttack.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerDetector))]
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject _weapon;
+    private PlayerDetector _playerDetector;
+
+    private void Start()
+    {
+        _playerDetector = GetComponent<PlayerDetector>();
+    }
+
+    private void Update()
+    {
+        if (!_playerDetector.IsDetectEnemy())
+        {
+            return;
+        }
+        
+        
+    }
+}
