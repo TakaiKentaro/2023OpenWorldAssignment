608f1528df0e04349885b72680d5c57e9a249fe9
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-06-14 21:40:20 +0900

    [add]PlayerDetector作成

---
+++ Assets/Scripts/Actor/Player/PlayerDetector.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    [SerializeField] private float _detectionRadius = 5f;
+    [SerializeField] private LayerMask _enemyLayer;
+
+    private bool _isDetected;
+    private bool _wasDetected; // 前回の検知状態を保存する変数
+
+    private void Update()
+    {
+        DetectEnemy();
+    }
+
+    private void DetectEnemy()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _detectionRadius, _enemyLayer);
+        bool detected = colliders.Length > 0;
+
+        if (detected && !_wasDetected)
+        {
+            _isDetected = true;
+            Debug.Log("敵発見");
+        }
+        else if (!detected && _wasDetected)
+        {
+            _isDetected = false;
+            Debug.Log("敵不明");
+        }
+
+        _wasDetected = detected; // 前回の検知状態を更新
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _detectionRadius);
+    }
+
+    public bool IsDetectEnemy()
+    {
+        return _isDetected;
+    }
+}
